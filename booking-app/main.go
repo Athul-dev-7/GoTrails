9028f42de4edcfae45dde5b54c5264d868dca464
Das <athuldas@hamon.in>
2022-08-14 11:54:09 +0530

    Infinite for loops.

--- booking-app/main.go
+++ booking-app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
@@ -15,29 +16,38 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still remaining.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend ")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	// Ask user for their details
-	fmt.Println("Enter your first name")
-	fmt.Scan(&firstName)
+		// Ask user for their details
+		fmt.Println("Enter your first name")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your last name")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter your last name")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter your email address")
-	fmt.Scan(&email)
+		fmt.Println("Enter your email address")
+		fmt.Scan(&email)
 
-	fmt.Println("Enter the number of tickets")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter the number of tickets")
+		fmt.Scan(&userTickets)
 
-	remainingTickets = remainingTickets - int(userTickets)
-	bookings = append(bookings, firstName+" "+lastName)
+		remainingTickets = remainingTickets - int(userTickets)
+		bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("Thankyou %v %v for booking %v tickets. You will receive a confirmation mail at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v remaining tickets for %v\n", remainingTickets, conferenceName)
+		fmt.Printf("Thankyou %v %v for booking %v tickets. You will receive a confirmation mail at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v remaining tickets for %v\n", remainingTickets, conferenceName)
 
-	fmt.Printf("These are all our bookings :%v\n", bookings)
+		firstNames := []string{}
+		for _, booking := range bookings {
+			names := strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+
+		fmt.Printf("The first names of bookings are : %v\n", firstNames)
+	}
+
 }
